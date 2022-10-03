0043a1da4268de76bcb73070a46408d546db6937
Manuel Benini <manuelbenini1905@gmail.com>
2022-10-03 16:45:32 +0200

    Terminati esercizi da 1 a 6 e stampati in pagina

---
+++ Controllers/EserciziController.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class EserciziController : Controller
+    {
+        private readonly ILogger<EserciziController> _logger;
+
+        public EserciziController(ILogger<EserciziController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
